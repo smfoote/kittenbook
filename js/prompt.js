5e92e9c1cbd8094af98705e8c672d25fec97f8fc
Steven Foote <smfoote@gmail.com>
2014-09-06 14:52:00 -0700

    Save the area code in a variable

--- js/prompt.js
+++ js/prompt.js
@@ -4,6 +4,10 @@ var userName = prompt('Hello, what\'s your name?');
 var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
 // Create the phone number pattern.
 var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
+// Get matches from phoneNumber var phoneMatches = phoneNumberPattern.exec(phoneNumber);
+// If the phone number is 901-555-5309, then phoneMatches will be
+// ['901-555-5309', '901']
+var areaCode = phoneMatches[1];
 // Create a variable to store the output.
 var output = '<h1>Hello, ' + userName + '!</h1>';
 
