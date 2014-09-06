e7bb371125516bc7678b776fed0fc9fed29f39aa
Steven Foote <smfoote@gmail.com>
2014-09-06 14:10:13 -0700

    Add Gruntfile.js (that doesn't do anything yet)

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = funciton(grunt){
+    // Project configuration
+    grunt.initConfig({
+      /*
+       * We will configure our tasks here
+       */
+    });
+
+    // We will load Grunt plugins here
+
+    // We will register tasks here
+};
