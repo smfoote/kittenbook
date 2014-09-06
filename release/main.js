31900144ba9146fff3ae59665502055d58ba7b89
Steven Foote <smfoote@gmail.com>
2014-09-06 14:42:41 -0700

    Add phone number validation

--- release/main.js
+++ release/main.js
@@ -16,7 +16,25 @@ var currentTime = currentDate.getFullYear() + '-' +  // Set year
 
 kbValues.currentTime = currentTime;
 
+// Get the user's name.
 var userName = prompt('Hello, what\'s your name?');
-document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
-                         '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
-                         ' accessed on: ' + kbValues.currentTime + '</p>';
+// Get the user's phone number.
+var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
+// Create the phone number pattern.
+var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+// Create a variable to store the output.
+var output = '<h1>Hello, ' + userName + '!</h1>';
+
+// Is the phone number valid?
+if (phoneNumberPattern.test(phoneNumber)) {
+
+  // Yes, the phone number is valid! Add the success message to the output.
+  output = output + '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
+           ' viewed on: ' + kbValues.currentTime + '</p>';
+
+} else {
+  // No, the phone number is not valid. Tell the user about the problem.
+  output = output + '<h2>That phone number is invalid: ' + phoneNumber;
+}
+// Insert the output into the web page.
+document.body.innerHTML = output;
