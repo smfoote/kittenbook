f577b340f2f0ffd29a118d6a7a29a5a605cad902
Steven Foote <smfoote@gmail.com>
2014-09-06 14:19:35 -0700

    Add grunt watch task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,31 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      concat: {
-        release: {
-          src: ['js/values.js', 'js/prompt.js'],
-          dest: 'release/main.js'
+        concat: {
+          release: {
+            src: ['js/values.js', 'js/prompt.js'],
+            dest: 'release/main.js'
+          }
+        },
+        copy: {
+          release: {
+            src: 'manifest.json',
+            dest: 'release/manifest.json'
+          }
+        },
+        jshint: {
+          files: ['js/values.js', 'js/prompt.js']
+        },
+        watch: {
+          files: ['<%= jshint.files %>', 'manifest.json'],
+          tasks: ['default']
         }
-      },
-      copy: {
-        release: {
-          src: 'manifest.json',
-          dest: 'release/manifest.json'
-        }
-      },
-      jshint: {
-        files: ['js/values.js', 'js/prompt.js']
-      }
     });
 
     // Load Grunt plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Register tasks
     grunt.registerTask('default', ['jshint', 'concat', 'copy']);
