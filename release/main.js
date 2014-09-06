1ec7874d78ca9e4f28f423e969db54d8c916548a
Steven Foote <smfoote@gmail.com>
2014-09-06 14:45:03 -0700

    Capture the area code

--- release/main.js
+++ release/main.js
@@ -21,7 +21,7 @@ var userName = prompt('Hello, what\'s your name?');
 // Get the user's phone number.
 var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
 // Create the phone number pattern.
-var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
 // Create a variable to store the output.
 var output = '<h1>Hello, ' + userName + '!</h1>';
 
