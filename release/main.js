ee95678ad72e8fb235feed5d1938480ab7465b5a
Steven Foote <smfoote@gmail.com>
2014-09-06 19:52:42 -0700

    Add js/getImages.js and update Gruntfile.js to include it
    Conflicts:
    	release/main.js

--- release/main.js
+++ release/main.js
@@ -46,3 +46,5 @@ if (phoneNumberPattern.test(phoneNumber)) {
 }
 // Insert the output into the web page.
 document.body.innerHTML = output;
+
+var images = document.querySelectorAll('div.userContentWrapper img');
