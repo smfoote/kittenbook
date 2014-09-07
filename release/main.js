403177270fdef3312deb18b8645f514170f53f2a
Steven Foote <smfoote@gmail.com>
2014-09-06 20:51:48 -0700

    Move area code capture inside the if block

--- release/main.js
+++ release/main.js
@@ -30,16 +30,16 @@ var userName = prompt('Hello, what\'s your name?');
 var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
 // Create the phone number pattern.
 var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
-// Get matches from phoneNumber
-var phoneMatches = phoneNumberPattern.exec(phoneNumber);
-// If the phone number is 901-555-5309, then phoneMatches will be
-// ['901-555-5309', '901']
-var areaCode = phoneMatches[1];
 // Create a variable to store the output.
 var output = '<h1>Hello, ' + userName + '!</h1>';
 
 // Is the phone number valid?
 if (phoneNumberPattern.test(phoneNumber)) {
+  // Get matches from phoneNumber
+  var phoneMatches = phoneNumberPattern.exec(phoneNumber);
+  // If the phone number is 901-555-5309, then phoneMatches will be
+  // ['901-555-5309', '901']
+  var areaCode = phoneMatches[1];
 
   // Yes, the phone number is valid! Add the success message to the output.
   output = output + '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
