a1efe929e37d93a1bb55a852e2ea3979bcef92a8
Steven Foote <smfoote@gmail.com>
2014-09-06 22:00:20 -0700

    Add functions to prompt.js

--- release/main.js
+++ release/main.js
@@ -25,42 +25,46 @@ var currentTime = currentDate.getFullYear() + '-' +  // Set year
 kbValues.currentTime = currentTime;
 
 // Get the user's name.
-var userName = prompt('Hello, what\'s your name?');
+function getUserName() {
+  var userName = prompt('Hello, what\'s your name?');
 
-// If no user name was entered, ask again.
-if (!userName) {
-  userName = prompt('You didn\'t enter a name. Really, what\'s your name?');
+  if (!userName) {
+    userName = prompt('You didn\'t enter a name. Really, what\'s your name?');
+  }
+  return userName;
 }
 
 // Get the user's phone number.
-var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
-// Create the phone number pattern.
-var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
-// Get matches from phoneNumber
-var phoneMatches = phoneNumberPattern.exec(phoneNumber);
-// Create a variable to store the output.
-var output = '<h1>Hello, ' + userName + '!</h1>';
+function getPhoneNumber(userName) {
+  var phoneNumber = prompt('Hello ' + userName +', what\'s your phone number?');
+  if (!validatePhoneNumber(phoneNumber)) {
+    phoneNumber = prompt('Please enter a valid phone number.');
+  }
+  return phoneNumber;
+}
 
-// Is the phone number valid?
-if (phoneNumberPattern.test(phoneNumber)) {
+// Validate a phone number
+function validatePhoneNumber(phoneNumber) {
+  return phoneNumber.match(/(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/);
+}
+
+// Determine location based on phone number
+function getLocation(phoneNumber) {
+  // Create the phone number pattern.
+  var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
   // Get matches from phoneNumber
   var phoneMatches = phoneNumberPattern.exec(phoneNumber);
-  // If the phone number is 901-555-5309, then phoneMatches will be
-  // ['901-555-5309', '901']
-  var areaCode = phoneMatches[1];
-  // Get the location using bracket syntax
-  var userLocation = kbValues.areaCodes[areaCode];
+  var areaCodes, areaCode, locationName;
+  if (phoneMatches) {
+    areaCode = phoneMatches[1];
+    areaCodes = getAreaCodes();
+    locationName = areaCodes[areaCode];
+  }
 
-  // Yes, the phone number is valid! Add the success message to the output.
-  output = output + '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
-           ' viewed on: ' + kbValues.currentTime + '</p>';
-
-} else {
-  // No, the phone number is not valid. Tell the user about the problem.
-  output = output + '<h2>That phone number is invalid: ' + phoneNumber;
+  // Look, it’s a ternary operator.
+  // Return the locationName if it exists, else return ‘somewhere’
+  return locationName ? locationName : 'somewhere';
 }
-// Insert the output into the web page.
-// document.body.innerHTML = output;
 
 var images = document.querySelectorAll('div.userContentWrapper img');
 
