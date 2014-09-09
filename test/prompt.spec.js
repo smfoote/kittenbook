d93c1e9aed71cba623a3b280a05da78157f5d09e
Steven Foote <smfoote@gmail.com>
2014-09-08 20:14:53 -0700

    Add a failing test for validatePhoneNumber

--- test/prompt.spec.js
+++ test/prompt.spec.js
@@ -7,5 +7,14 @@ describe('prompt.js', function() {
       expect(typeof result).toBe('boolean');
     });
 
+    it('should return true when given a 1-800 number', function() {
+      var result = validatePhoneNumber('1-800-867-5309');
+
+      // Change to the following when we know this test can fail:
+      //
+      // expect(result).toBe(true);
+      //
+      expect(result).toBe(false);
+    });
   });
 });
